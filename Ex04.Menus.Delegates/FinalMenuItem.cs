81b9ad0424b025a9999bc9eb7022a2979f02983c
Omer Zidkoni <50792403+omerzi@users.noreply.github.com>
2020-06-06 14:00:04 +0300

    Starting with Delegates Project

---
+++ Ex04.Menus.Delegates/FinalMenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Delegates
+{
+    public class FinalMenuItem : MenuItem
+    {
+        public event ActivateHandler HasActivated;
+
+        public FinalMenuItem(string i_Name) : base(i_Name)
+        {
+        }
+
+        protected virtual void OnActivation()
+        {
+            HasActivated?.Invoke();
+        }
+    }
+}
---
+++ Ex04.Menus.Delegates/MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Delegates
+{
+    public class MenuItem
+    {
+        private readonly List<MenuItem> r_SubMenu = null;
+        private string m_ItemName;
+
+        public MenuItem(string i_Name)
+        {
+            m_ItemName = i_Name;
+        }
+
+        public List<MenuItem> SubMenu
+        {
+            get { return r_SubMenu; }
+        }
+
+        public string ItemName
+        {
+            get { return m_ItemName; }
+            set 
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new NullReferenceException("A name cannot be empty or consist of white-spaces only!");
+                }
+
+                m_ItemName = value;
+            }
+        }
+    }
+}
